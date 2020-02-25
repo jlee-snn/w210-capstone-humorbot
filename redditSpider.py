87c2dec2e9475f951f8989ae414fa930fa7a23c3
Joseph Lee <jle609@berkeley.edu>
2020-02-24 20:35:51 -0600

    Scrapy version script and data

--- redditSpider.py
+++ redditSpider.py
@@ -11,12 +11,12 @@ class RedditspiderSpider(scrapy.Spider):
     start_urls = ['https://www.reddit.com/r/jokes/']
 
     custom_settings = {
-        'DEPTH_LIMIT': 10
+        'DEPTH_LIMIT': 100
     }
 
     def parse(self, response):
         titles = response.xpath('//*[@class="_eYtD2XCVieq6emjKBH3m"]/text()').extract()
-        body = response.xpath('//span[@class="Chtkt3BCZQruf0LtmFg2c"]/text()').extract() #need to fix this one
+        body = response.xpath('//*[@class="_1qeIAgB0cPwnLhDF9XSiJM"]/text()').extract()
         datetimes = response.xpath('//*[@data-click-id="timestamp"]/text()').extract()
         for (title,body,datetime) in zip(titles,body,datetimes):
             yield {'Title': title.encode('utf-8'),'Date Time': datetime,'Body':body}
